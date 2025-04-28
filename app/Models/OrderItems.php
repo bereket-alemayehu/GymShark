9f1f23a23fa59f2d2aff8ac5dcae1532ced11f29
TayeAnimaw <tayeanimaw7@gmail.com>
2025-04-28 15:12:21 +0300

    all are setted up

--- app/Models/OrderItems.php
+++ app/Models/OrderItems.php
@@ -14,10 +14,10 @@ class OrderItems extends Model
         return $this->belongsTo(Order::class);
     }
 
-    public function product()
-    {
-        return $this->belongsTo(Product::class);
-    }
+    // public function product()
+    // {
+    //     return $this->belongsTo(Product::class);
+    // }
 
     public function variant()
     {
