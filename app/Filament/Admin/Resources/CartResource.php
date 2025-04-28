aa73040d3d5f2c9ccce09d35297f0899692acb3c
TayeAnimaw <tayeanimaw7@gmail.com>
2025-04-28 15:34:41 +0300

    cart and cart items are updated

---
+++ app/Filament/Admin/Resources/CartResource.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Filament\Admin\Resources;
+
+use App\Filament\Admin\Resources\CartResource\Pages;
+use App\Filament\Admin\Resources\CartResource\RelationManagers;
+use App\Models\Cart;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CartResource extends Resource
+{
+    protected static ?string $model = Cart::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationGroup = 'payment & Order Management';
+    protected static ?int $navigationSort = 4;
+    protected static ?string $navigationLabel = 'Carts';
+    protected static ?string $label = 'Cart';
+    protected static ?string $pluralLabel = 'Carts';
+    protected static ?string $title = 'Cart';
+    protected static ?string $slug = 'carts';
+    public static function getNavigationBadge(): ?string
+    {
+        return Cart::count();
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('user_id')
+                    ->required()
+                    ->relationship('user', 'name'),
+                Forms\Components\TextInput::make('status')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('user.name')
+                    ->label('User')
+                    ->sortable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('status'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make()
+                    ->label('Edit')
+                    ->icon('heroicon-o-pencil')
+                    ->color('primary'),
+                Tables\Actions\DeleteAction::make()
+                    ->label('Delete')
+                    ->icon('heroicon-o-trash')
+                    ->color('danger')
+                    ->action(function (Cart $record) {
+                        $record->delete();
+                        return redirect()->route('filament.resources.carts.index');
+                    }),
+            ])
+
+
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCarts::route('/'),
+            'create' => Pages\CreateCart::route('/create'),
+            'edit' => Pages\EditCart::route('/{record}/edit'),
+        ];
+    }
+}
