4b38052c782d22e8a333caf77b1d0df6755788b9
TayeAnimaw <tayeanimaw7@gmail.com>
2025-04-29 10:27:56 +0300

    all are setted up on the notification

--- app/Observers/NotificationObserver.php
+++ app/Observers/NotificationObserver.php
@@ -13,11 +13,16 @@ class NotificationObserver
      */
     public function created(User $user): void
     {
-        $payment = Auth::user();
+        $authUser = Auth::user();
+
+        if (!$authUser) {
+            return; // Don't send notification if no user is authenticated
+        }
+
         Notification::make()
             ->title('Order Placed')
             ->body('Your order has been placed successfully')
-            ->sendToDatabase($payment);
+            ->sendToDatabase($authUser);
     }
 
     /**
